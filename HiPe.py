16efeac84436699cec513399356b704e551f5b38
Jessica <jessica@jmcooper.net>
2021-07-13 13:32:48 +0100

    tidied up threshold

--- HiPe.py
+++ HiPe.py
@@ -44,7 +44,7 @@ def hierarchical_perturbation(model,
             b_list = []
             num_cells *= 2
             depth += 1
-            threshold = (torch.max(saliency) + torch.min(saliency)) / 2
+            threshold = torch.min(saliency) + ((torch.max(saliency) - torch.min(saliency)) / 2)
 
             # print('Depth: {}, {} x {} Cell'.format(depth, input_y_dim//num_cells, input_x_dim//num_cells))
             # print('Threshold: {:.1f}'.format(threshold))
